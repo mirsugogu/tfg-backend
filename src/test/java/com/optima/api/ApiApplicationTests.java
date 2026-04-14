69c21db5b0e725018a0ddb588f99c511996372cd
Mirtha Gomez <msgomezguadalupe@gmail.com>
2026-04-14 23:35:03 +0200

    init springboot project

---
+++ src/test/java/com/optima/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optima.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
