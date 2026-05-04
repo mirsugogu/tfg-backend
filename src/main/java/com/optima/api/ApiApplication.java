b2a921838620b711c4d8dc460815dbbd0a2d7213
Alejandro <git config --global user.email diegoalejandro.barrerom@iesfm.onmicrosoft.com>
2026-05-04 22:05:37 +0200

    Configuración de conexión y application.properties

--- src/main/java/com/optima/api/ApiApplication.java
+++ src/main/java/com/optima/api/ApiApplication.java
@@ -3,27 +3,16 @@ package com.optima.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
+/**
+ * Punto de entrada de la aplicación.
+ * Toda la configuración (datasource, JPA, seguridad, etc.) vive en
+ * src/main/resources/application.properties; el main se mantiene limpio.
+ */
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(ApiApplication.class);
-		Properties props = new Properties();
-
-		// Base de Datos
-		props.setProperty("spring.datasource.url", "jdbc:mysql://localhost:3307/optima_db");
-		props.setProperty("spring.datasource.username", "optima_user");
-		props.setProperty("spring.datasource.password", "optima_pass");
-
-		// SEGURIDAD (Esto quita el 403)
-		props.setProperty("spring.main.allow-bean-definition-overriding", "true");
-		props.setProperty("spring.jpa.hibernate.ddl-auto", "none");
-		props.setProperty("spring.security.csrf.disable", "true");
-
-		app.setDefaultProperties(props);
-		app.run(args);
+		SpringApplication.run(ApiApplication.class, args);
 	}
 
 }
